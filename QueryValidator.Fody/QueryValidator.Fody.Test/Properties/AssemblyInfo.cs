72d3000b7594ef52e7bbf318a6176a9f7942dc91
Kamil Mrzygłód <kmrzyglod@moneygram.com>
2016-05-11 10:44:17 +0200

    Solution cleaned

--- QueryValidator.Fody/QueryValidator.Fody.Test/Properties/AssemblyInfo.cs
+++ QueryValidator.Fody/QueryValidator.Fody.Test/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("QueryValidator.Fody.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Codenova.pl")]
 [assembly: AssemblyProduct("QueryValidator.Fody.Test")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Codenova.pl 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
